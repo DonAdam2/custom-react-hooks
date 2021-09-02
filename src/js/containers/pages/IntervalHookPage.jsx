ffaddd6c4f0dadebd87a640ba4796e728dffdce8
Adam Morsi <adam.morsi@yahoo.com>
2021-09-02 17:36:15 +0300

    use useInterval hook

--- src/js/containers/pages/IntervalHookPage.jsx
+++ src/js/containers/pages/IntervalHookPage.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+//custom hooks
+import useInterval from '../../customHooks/UseInterval';
 
 const IntervalHookPage = () => {
-	return <div>IntervalHookPage</div>;
+	const [counter, setCounter] = useState(30);
+
+	const onStopHandler = () => {
+		clearInterval(intervalRef.current);
+	};
+
+	const onResetHandler = () => {
+		setCounter(30);
+	};
+
+	const intervalRef = useInterval(() => {
+		if (counter > 1) {
+			setCounter((prev) => prev - 1);
+		} else {
+			onResetHandler();
+		}
+	}, 1000);
+
+	return (
+		<div className="magnify-container">
+			<p>Counter {counter}</p>
+			<button onClick={onResetHandler}>Reset counter</button>
+			<button onClick={onStopHandler}>Stop counter</button>
+		</div>
+	);
 };
 
 export default IntervalHookPage;
