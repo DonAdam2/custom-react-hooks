b66a753138db50d44e66bea304bc9c8e595e16a9
DonAdam2 <adam.morsi@yahoo.com>
2023-02-13 13:35:19 +0200

    update enter esc events page

--- src/js/containers/pages/EnterEscEventsPage.jsx
+++ src/js/containers/pages/EnterEscEventsPage.jsx
@@ -1,15 +1,17 @@
 import { useEnterEscButtonsHook } from '@/js/customHooks/useEnterEscEvents';
+import { useCallback } from 'react';
 
 const EnterEscEventsPage = () => {
+  const cancelHandler = useCallback(() => {
+    console.log('Cancel function triggered on esc');
+  }, []);
+
+  const confirmHandler = useCallback(() => {
+    console.log('Confirm function triggered on enter');
+  }, []);
+
   useEnterEscButtonsHook({ cancelHandler, confirmHandler });
 
-  function cancelHandler() {
-    console.log('Cancel function triggered on esc');
-  }
-
-  function confirmHandler() {
-    console.log('Confirm function triggered on enter');
-  }
   return (
     <div className="magnify-container">
       Open the console and press enter or esc buttons on your keyboard to see events triggered
