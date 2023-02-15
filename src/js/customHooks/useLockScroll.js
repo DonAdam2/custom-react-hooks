5d050b0ac32b3c2c0301f2c2affffd995f6a9c39
DonAdam2 <adam.morsi@yahoo.com>
2023-02-15 16:07:49 +0200

    update use lock scroll hook

--- src/js/customHooks/useLockScroll.js
+++ src/js/customHooks/useLockScroll.js
@@ -3,13 +3,13 @@ import { useLayoutEffect } from 'react';
 function useLockScroll({ targetElement = document.body, immediate = true }) {
   useLayoutEffect(() => {
     // Get original element overflow
-    const originalStyle = window.getComputedStyle(targetElement).overflow;
-    if (immediate) {
+    const originalStyle = targetElement ? window.getComputedStyle(targetElement).overflow : 'auto';
+    if (immediate && targetElement) {
       // Prevent scrolling on mount
       targetElement.style.overflow = 'hidden';
     }
     return () => {
-      if (immediate) {
+      if (immediate && targetElement) {
         // Re-enable scrolling when component unmounts
         targetElement.style.overflow = originalStyle;
       }
