a3d86f0309af45074ea50a279d5a4365120255e8
DonAdam2 <adam.morsi@yahoo.com>
2023-04-12 13:13:50 +0300

    update useExternalScript hook

--- src/js/customHooks/useExternalScript.js
+++ src/js/customHooks/useExternalScript.js
@@ -2,22 +2,6 @@ import { useEffect, useRef, useState } from 'react';
 //constants
 import { availableExternalEntityStatuses } from '@/js/constants/AppConstants';
 
-//typescript
-/*export type status = 'idle' | 'loading' | 'ready' | 'error';
-
-export interface ExternalEntityInterface {
-  src: string;
-  immediate?: boolean;
-  attrs?: AttrsInterface;
-}
-
-export interface AttrsInterface {
-  [key: string]: string;
-}
-
-function setAttributes(script: HTMLScriptElement, attrs: AttrsInterface) {
-*/
-
 function setAttributes(script, attrs) {
   for (const attr in attrs) {
     script.setAttribute(attr, attrs[attr]);
@@ -96,3 +80,79 @@ function useExternalScript({ src, immediate = true, attrs }) {
 }
 
 export default useExternalScript;
+
+//typescript
+/*export type status = 'idle' | 'loading' | 'ready' | 'error';
+
+export interface ExternalEntityInterface {
+  src: string;
+  immediate?: boolean;
+  attrs?: AttrsInterface;
+}
+
+export interface AttrsInterface {
+  [key: string]: string;
+}
+
+
+function setAttributes(script: HTMLScriptElement, attrs: AttrsInterface) {
+  for (const attr in attrs) {
+    script.setAttribute(attr, attrs[attr]);
+  }
+}
+function useExternalScript({ src, immediate = true, attrs }: ExternalEntityInterface) {
+  const [status, setStatus] = useState<status>(
+      src ? availableExternalEntityStatuses.loading : availableExternalEntityStatuses.idle
+    ),
+    attrsRef = useRef(attrs);
+  useEffect(() => {
+    if (!src || !immediate) {
+      setStatus(availableExternalEntityStatuses.idle);
+      return;
+    }
+    let script: HTMLScriptElement | null = document.querySelector(`script[src="${src}"]`);
+    if (!script) {
+      script = document.createElement('script');
+      script.src = src;
+      script.async = true;
+      script.setAttribute('data-status', availableExternalEntityStatuses.loading);
+      document.body.appendChild(script);
+      const setAttributeFromEvent = (event: any) => {
+        if (script) {
+          script.setAttribute(
+            'data-status',
+            event.type === 'load'
+              ? availableExternalEntityStatuses.ready
+              : availableExternalEntityStatuses.error
+          );
+        }
+      };
+      script.addEventListener('load', setAttributeFromEvent);
+      script.addEventListener('error', setAttributeFromEvent);
+    } else {
+      const currentStatus = script.getAttribute('data-status') as status;
+      setStatus(currentStatus);
+    }
+    const setStateFromEvent = (event: any) => {
+      setStatus(
+        event.type === 'load'
+          ? availableExternalEntityStatuses.ready
+          : availableExternalEntityStatuses.error
+      );
+    };
+    if (script) {
+      attrsRef.current && setAttributes(script, attrsRef.current);
+      script.addEventListener('load', setStateFromEvent);
+      script.addEventListener('error', setStateFromEvent);
+    }
+    return () => {
+      if (script && immediate) {
+        script.removeEventListener('load', setStateFromEvent);
+        script.removeEventListener('error', setStateFromEvent);
+      }
+    };
+  }, [src, immediate]);
+  return status;
+}
+
+export default useExternalScript;*/
