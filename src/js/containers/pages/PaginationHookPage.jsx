8f7680db9d8f494e7046e46ad7675e6b00f0e6e8
“ <adam.morsi@yahoo.com>
2024-03-01 18:47:36 +0200

    update PaginationHookPage

--- src/js/containers/pages/PaginationHookPage.jsx
+++ src/js/containers/pages/PaginationHookPage.jsx
@@ -8,7 +8,7 @@ import Pagination from '../../components/shared/Pagination';
 
 const PaginationHookPage = () => {
   const /*[selectedOption, setSelectedOption] = useState('5'),*/
-    paginationData = usePagination({
+    { firstContentIndex, lastContentIndex, ...paginationData } = usePagination({
       // contentPerPage: +selectedOption,
       contentPerPage: 3,
       count: people.length,
@@ -33,22 +33,21 @@ const PaginationHookPage = () => {
         This hook gives you the pagination functionality out of the box, which can be integrated
         with any pagination component
       </p>
-      {people
-        .slice(paginationData.firstContentIndex, paginationData.lastContentIndex)
-        .map((el, i) => (
-          <Person
-            key={i}
-            id={i}
-            firstName={el.first_name}
-            lastName={el.last_name}
-            jobTitle={el.employment.title}
-            status={el.subscription.status}
-          />
-        ))}
+      {people.slice(firstContentIndex, lastContentIndex).map((el, i) => (
+        <Person
+          key={i}
+          id={i}
+          firstName={el.first_name}
+          lastName={el.last_name}
+          jobTitle={el.employment.title}
+          status={el.subscription.status}
+        />
+      ))}
 
       <Pagination
         // toolTipText={{ prevThreePages: 'Prev 3', nextThreePages: 'Next 3' }}
         // activePageColors={{ lightColor: '#fbd34d', darkColor: '#78350f' }}
+        // isLoading={true}
         {...paginationData}
       />
       {/*<select value={selectedOption} onChange={handleChange}>
