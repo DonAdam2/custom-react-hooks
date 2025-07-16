6f710108967f9ba033bb4906b6e7328411818f67
“ <adam.morsi@yahoo.com>
2025-07-16 14:51:52 +0300

    update devtool to eval

--- buildTools/webpack.dev.js
+++ buildTools/webpack.dev.js
@@ -14,7 +14,7 @@ const { merge } = require('webpack-merge'),
 module.exports = (env, options) => {
   return merge(common(env, options), {
     mode: 'development',
-    devtool: 'inline-source-map',
+    devtool: 'eval',
     //required for hot reload
     target: 'web',
     devServer: {
